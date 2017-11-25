afdad2e958de0d332b2e9a8f204a84c533c00965
komisarczyk <komisarczykkonrad@gmail.com>
2017-11-25 02:38:47 +0100

    wstępnie napisałem pie.h i walidacje w bakery.h

--- bakery.h
+++ bakery.h
@@ -1,7 +1,149 @@
 #ifndef JNP1ZAD4_BAKERY_H
 #define JNP1ZAD4_BAKERY_H
 
+#include <utility>
+#include "pie.h"
 
+
+/**
+ * Provides the member constant value equal to sum of areas of all bakery products in bakery.
+ * @tparam A type of shelf area
+ * @tparam P type of first bakery product
+ * @tparam Other types of other bakery products
+ */
+template<typename A, typename P, typename... Other>
+struct SumOfAreas {
+public:
+    //calculate sum of areas recursively
+    static constexpr A value = P::getArea() + SumOfAreas<A, Other...>::value;
+};
+
+/**
+ * Specialization for only one bakery product - base case.
+ */
+template<typename A, typename P>
+struct SumOfAreas<A, P> {
+public:
+    //base case
+    static constexpr A value = P::getArea();
+};
+
+
+
+/**
+ * Provides the member constant value equal to true, if type @tparam P is distinct from all other types (@tparam Q and @tparam Other)
+ * Otherwise value == false
+ */
+template<typename P, typename Q, typename... Other>
+struct IsFirstUnique {
+public:
+    //recursive formula
+    static constexpr bool value = (IsFirstUnique<P, Q>::value) && (IsFirstUnique<P, Other...>::value);
+};
+
+/**
+ * Specialization for only 1 other type - base case
+ */
+template<typename P, typename Q>
+struct IsFirstUnique<P, Q> {
+public:
+    //base case
+    static constexpr bool value = !std::is_same<P, Q>::value;
+};
+
+/**
+ * Provides the member constant value equal to true, if all types (@tparam P @tparam Q @tparam Other) are pairwise distinct
+ * O(n^2), where n is number of tparams
+ */
+template<typename P, typename Q = void, typename... Other>
+struct AreUnique {
+public:
+    //recursive formula
+    static constexpr bool value = (IsFirstUnique<P, Q, Other...>::value) && (AreUnique<Q, Other...>::value);
+};
+
+/**
+ * Specialization for only 2 types - base case
+ */
+template<typename P, typename Q>
+struct AreUnique<P, Q> {
+public:
+    static constexpr bool value = !std::is_same<P, Q>::value;
+};
+
+/**
+ * Specialization for only 1 type
+ */
+template<typename P>
+struct AreUnique<P> {
+public:
+    static constexpr bool value = true;
+};
+
+
+/**
+ * Checks if all of bakery products (@tparam P and @tparam Other) have size type equal to @tparam A.
+ */
+template<typename A, typename P, typename... Other>
+struct HaveGoodSizeType {
+public:
+    static constexpr bool value = (HaveGoodSizeType<A, P>::value) && (HaveGoodSizeType<A, Other...>::value);
+};
+
+/**
+ * Specialization for only 1 product type
+ */
+template<typename A, typename P>
+struct HaveGoodSizeType<A, P> {
+public:
+    static constexpr bool value = std::is_same<A, typename P::SizeType>::value;
+};
+
+
+/**
+ * Checks if all of bakery products (@tparam P and @tparam Other) have size type equal to @tparam A.
+ */
+template<typename C, typename P, typename... Other>
+struct HaveGoodPriceType {
+public:
+    static constexpr bool value = (HaveGoodPriceType<C, P>::value) && (HaveGoodPriceType<C, Other...>::value);
+};
+
+/**
+ * Specialization for only 1 product type
+ */
+template<typename C, typename P>
+struct HaveGoodPriceType<C, P> {
+public:
+    static constexpr bool value = (!P::isSellable) || (std::is_same<C, typename P::PriceType>::value);
+    //TODO - trzeba zrobić jakoś, żeby się ewaluowało leniwie, albo wypiek zawsze miał PriceType
+};
+
+
+template<typename C, typename A, A shelfArea, typename... P>
+class Bakery {
+    static_assert(std::is_floating_point<C>::value,
+                  "C type has to be floating point");
+    static_assert(std::is_integral<A>::value,
+                  "A type has to be integral");
+    static_assert(HaveGoodSizeType<A, P...>::value,
+                  "typ wymiaru wszystkich produktów w piekarni musi być taki sam jak typ A");
+    //static_assert(HaveGoodPriceType<C, P...>::value,
+      //            "typ ceny wszystkich produktów (przeznaczonych na sprzedaż) w piekarni musi być taki sam jak typ C");
+    static_assert(SumOfAreas<A, P...>::value <= shelfArea,
+                  "produkty w piekarni nie mogą mieć łącznej powierzchni większej niż przestrzeń na półkach");
+    static_assert(AreUnique<P...>::value,
+                  "typy zawarte w ramach parametru P muszą być unikatowe");
+
+private:
+    C profits;
+public:
+    Bakery(P... products) {
+        profits = 0;
+    }
+
+
+};
 
 
 
--- pie.h
+++ pie.h
@@ -2,6 +2,8 @@
 #define JNP1ZAD4_PIE_H
 
 #include <cmath>
+#include <cassert>
+#include <iostream>
 
 /**
  * @return Approximation of PI
@@ -28,6 +30,91 @@ constexpr double pi_approx(unsigned int n) {
  */
 constexpr double PI = pi_approx(8);
 
+
+
+
+template<typename R, R radius, bool isSellable, typename P = void>
+class Pie {
+};
+
+template<typename R, R radius>
+class Pie<R, radius, false> {
+    static_assert(std::is_integral<R>::value, "R type has to be integral");
+private:
+    int stock;
+
+public:
+    static constexpr bool isSellable = false;
+
+    typedef R SizeType;
+
+    Pie(int initialStock) :
+            stock(initialStock) {
+        assert(initialStock >= 1);
+    }
+
+    static constexpr double getArea() {
+        return (PI * radius * radius);
+    }
+
+    int getStock() {
+        return stock;
+    }
+};
+
+template<typename R, R radius, typename P>
+class Pie<R, radius, true, P> {
+    static_assert(std::is_integral<R>::value, "R type has to be integral");
+    static_assert(std::is_floating_point<P>::value, "P type has to be floating point");
+private:
+    int stock;
+    P price;
+public:
+    static constexpr bool isSellable = true;
+
+    typedef R SizeType;
+
+    typedef P PriceType;
+
+    Pie(int initialStock, P price) :
+            stock(initialStock),
+            price(price) {
+        assert(initialStock >= 1);
+    }
+
+    static constexpr double getArea() {
+        return (PI * radius * radius);
+    }
+
+    int getStock() {
+        return stock;
+    }
+
+    P getPrice() {
+        return price;
+    }
+
+    void sell() {
+        if (stock >= 1) {
+            stock--;
+        }
+        else {
+            std::cerr << "Stock is empty. Nothing to sell." << std::endl;
+        }
+    }
+};
+
+template<typename R, R radius>
+class CherryPie : public Pie<R, radius, false> {
+public:
+    CherryPie(int initialStock) : Pie<R, radius, false>(initialStock) {}
+};
+
+template<typename R, R radius, typename P>
+class ApplePie : public Pie<R, radius, true, P> {
+public:
+    ApplePie(int initialStock, P price) : Pie<R, radius, true, P>(initialStock, price) {}
+};
 
 
 #endif //JNP1ZAD4_PIE_H
