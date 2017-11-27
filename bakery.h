26c16c439e1764b996f30af29128e4a24b54e709
komisarczyk <komisarczykkonrad@gmail.com>
2017-11-27 21:04:10 +0100

    ciąg dalszy bakery.h

--- bakery.h
+++ bakery.h
@@ -2,6 +2,7 @@
 #define JNP1ZAD4_BAKERY_H
 
 #include <utility>
+#include <tuple>
 #include "pie.h"
 
 
@@ -12,17 +13,17 @@
  * @tparam Other types of other bakery products
  */
 template<typename A, typename P, typename... Other>
-struct SumOfAreas {
+struct sum_of_areas {
 public:
     //calculate sum of areas recursively
-    static constexpr A value = P::getArea() + SumOfAreas<A, Other...>::value;
+    static constexpr A value = P::getArea() + sum_of_areas<A, Other...>::value;
 };
 
 /**
  * Specialization for only one bakery product - base case.
  */
 template<typename A, typename P>
-struct SumOfAreas<A, P> {
+struct sum_of_areas<A, P> {
 public:
     //base case
     static constexpr A value = P::getArea();
@@ -35,17 +36,17 @@ public:
  * Otherwise value == false
  */
 template<typename P, typename Q, typename... Other>
-struct IsFirstUnique {
+struct is_first_unique {
 public:
     //recursive formula
-    static constexpr bool value = (IsFirstUnique<P, Q>::value) && (IsFirstUnique<P, Other...>::value);
+    static constexpr bool value = (is_first_unique<P, Q>::value) && (is_first_unique<P, Other...>::value);
 };
 
 /**
  * Specialization for only 1 other type - base case
  */
 template<typename P, typename Q>
-struct IsFirstUnique<P, Q> {
+struct is_first_unique<P, Q> {
 public:
     //base case
     static constexpr bool value = !std::is_same<P, Q>::value;
@@ -56,17 +57,17 @@ public:
  * O(n^2), where n is number of tparams
  */
 template<typename P, typename Q = void, typename... Other>
-struct AreUnique {
+struct are_unique {
 public:
     //recursive formula
-    static constexpr bool value = (IsFirstUnique<P, Q, Other...>::value) && (AreUnique<Q, Other...>::value);
+    static constexpr bool value = (is_first_unique<P, Q, Other...>::value) && (are_unique<Q, Other...>::value);
 };
 
 /**
  * Specialization for only 2 types - base case
  */
 template<typename P, typename Q>
-struct AreUnique<P, Q> {
+struct are_unique<P, Q> {
 public:
     static constexpr bool value = !std::is_same<P, Q>::value;
 };
@@ -75,7 +76,7 @@ public:
  * Specialization for only 1 type
  */
 template<typename P>
-struct AreUnique<P> {
+struct are_unique<P> {
 public:
     static constexpr bool value = true;
 };
@@ -85,16 +86,16 @@ public:
  * Checks if all of bakery products (@tparam P and @tparam Other) have size type equal to @tparam A.
  */
 template<typename A, typename P, typename... Other>
-struct HaveGoodSizeType {
+struct have_good_size_type {
 public:
-    static constexpr bool value = (HaveGoodSizeType<A, P>::value) && (HaveGoodSizeType<A, Other...>::value);
+    static constexpr bool value = (have_good_size_type<A, P>::value) && (have_good_size_type<A, Other...>::value);
 };
 
 /**
  * Specialization for only 1 product type
  */
 template<typename A, typename P>
-struct HaveGoodSizeType<A, P> {
+struct have_good_size_type<A, P> {
 public:
     static constexpr bool value = std::is_same<A, typename P::SizeType>::value;
 };
@@ -104,20 +105,54 @@ public:
  * Checks if all of bakery products (@tparam P and @tparam Other) have size type equal to @tparam A.
  */
 template<typename C, typename P, typename... Other>
-struct HaveGoodPriceType {
+struct have_good_price_type {
 public:
-    static constexpr bool value = (HaveGoodPriceType<C, P>::value) && (HaveGoodPriceType<C, Other...>::value);
+    static constexpr bool value = (have_good_price_type<C, P>::value) && (have_good_price_type<C, Other...>::value);
 };
 
 /**
  * Specialization for only 1 product type
  */
 template<typename C, typename P>
-struct HaveGoodPriceType<C, P> {
+struct have_good_price_type<C, P> {
 public:
     static constexpr bool value = (!P::isSellable) || (std::is_same<C, typename P::PriceType>::value);
     //TODO - trzeba zrobić jakoś, żeby się ewaluowało leniwie, albo wypiek zawsze miał PriceType
 };
+
+
+/**
+ * Provides field value equal to true if parameter pack @tparam First, P contains type @tparam Product
+ * Otherwise value is false
+ */
+template<typename Product, typename First, typename... P>
+struct contains {
+    static constexpr bool value = contains<Product, First>::value || contains<Product, P...>::value;
+};
+
+template<typename Product, typename First>
+struct contains<Product, First> {
+    static constexpr bool value = std::is_same<Product, First>::value;
+};
+
+
+/**
+ * Provides field value equal to first position of type @tparam Product in parameter pack @tparam First, P
+ */
+template<typename Product, typename First, typename... P>
+struct first_position {
+    //static_assert(contains<Product, First, P...>::value, ""); //TODO - dlaczego to nie działa?
+    static constexpr size_t value = std::is_same<Product, First>::value ? 0 : first_position<Product, P...>::value + 1;
+};
+
+template<typename Product, typename First>
+struct first_position<Product, First> {
+
+    static constexpr size_t value = 0;
+};
+
+
+
 
 
 template<typename C, typename A, A shelfArea, typename... P>
@@ -126,21 +161,79 @@ class Bakery {
                   "C type has to be floating point");
     static_assert(std::is_integral<A>::value,
                   "A type has to be integral");
-    static_assert(HaveGoodSizeType<A, P...>::value,
+    static_assert(have_good_size_type<A, P...>::value,
                   "typ wymiaru wszystkich produktów w piekarni musi być taki sam jak typ A");
-    //static_assert(HaveGoodPriceType<C, P...>::value,
+    //static_assert(have_good_price_type<C, P...>::value,
       //            "typ ceny wszystkich produktów (przeznaczonych na sprzedaż) w piekarni musi być taki sam jak typ C");
-    static_assert(SumOfAreas<A, P...>::value <= shelfArea,
+    static_assert(sum_of_areas<A, P...>::value <= shelfArea,
                   "produkty w piekarni nie mogą mieć łącznej powierzchni większej niż przestrzeń na półkach");
-    static_assert(AreUnique<P...>::value,
+    static_assert(are_unique<P...>::value,
                   "typy zawarte w ramach parametru P muszą być unikatowe");
 
 private:
+    /**
+     * Current Bakery's profits
+     */
     C profits;
-public:
-    Bakery(P... products) {
-        profits = 0;
+
+    /**
+     * Tuple storing Bakery's products
+     */
+    std::tuple<P...> productsTuple;
+
+    /**
+     * @return Bakery's product of type @tparam Product
+     */
+    template<typename Product>
+    Product& getProduct() {
+        static_assert(contains<Product, P...>::value,
+                      "podanie typu, który nie występuje na stanie piekarni musi powodować błąd kompilacji");
+
+        static constexpr size_t product_position = first_position<Product, P...>::value;
+        return std::get<product_position>(productsTuple);
     }
+
+public:
+    Bakery(P... products) :
+            profits(0),
+            productsTuple(products...) {}
+
+    C getProfits() {
+        return profits;
+    }
+
+    template<typename Product>
+    void sell() {
+        static_assert(contains<Product, P...>::value,
+                      "podanie typu, który nie występuje na stanie piekarni musi powodować błąd kompilacji");
+        static_assert(Product::isSellable,
+                      "Product isn't sellable.");
+
+        auto& product = getProduct<Product>();
+        if (product.getStock() >= 1) {
+            product.sell();
+            profits += product.getPrice();
+        }
+    }
+
+    template<typename Product>
+    int getProductStock() {
+        static_assert(contains<Product, P...>::value,
+                      "podanie typu, który nie występuje na stanie piekarni musi powodować błąd kompilacji");
+
+        return getProduct<Product>().getStock();
+    }
+
+    template<typename Product>
+    void restock(int additionalStock) {
+        static_assert(contains<Product, P...>::value,
+                      "podanie typu, który nie występuje na stanie piekarni musi powodować błąd kompilacji");
+        //TODO: musi być ApplePie
+
+        //TODO
+    }
+
+
 
 
 };
