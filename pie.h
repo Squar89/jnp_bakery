8dcbbf2c28df9a5e27e9c6dbd163d91ee69838ec
komisarczyk <komisarczykkonrad@gmail.com>
2017-11-30 19:38:33 +0100

    obliczanie pi bez sqrt

--- pie.h
+++ pie.h
@@ -7,28 +7,21 @@
 
 /**
  * @return Approximation of PI
- * Calculates perimeters of regular polygons inscribed and circumscribed on circle
- * Returns half of mean of perimeters of polygons of (6 * 2^(@param n)) sides
+ * Leibnitz`s method -
+ * Calculates (@param n)-th partial sum of Maclaurin`s series for function y = arctg(x) and x = 1
  */
 constexpr double pi_approx(unsigned int n) {
-    //perimeter of polygon INSCRIBED in unit circle. Starting with hexagon.
-    double insc = 6;
-    //perimeter of polygon CIRCUMSCRIBED on unit circle. Starting with hexagon.
-    double cirsc = 6 * 2 / sqrt(3); // TODO - sqrt też musimy samemu przybliżać?
-
+    double pi = 0;
     for (int i = 0; i < n; i++) {
-        // we use formula from Archimedes "Measurement of a Circle"
-        cirsc = 2 * insc * cirsc / (insc + cirsc);
-        insc = sqrt(insc * cirsc);
+        pi += 4 / (2 * (double) i + 1) * (1 - 2 * (i % 2));
     }
-
-    return (cirsc + insc) / 4;
+    return pi;
 }
 
 /**
  * Approximation of PI. Most accurate that it is possible in type double.
  */
-constexpr double PI = pi_approx(8);
+constexpr double PI = pi_approx(160000);
 
 template<typename R, R radius, bool isSellable, typename P = void>
 class Pie {
